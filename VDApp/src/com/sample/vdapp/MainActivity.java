b42aad00ad876ec0b7a1b4784f3b51177f0b4b5c
nitish.patel <nitish.patel@RIL15069CKB152.rolta.com>
2016-10-10 11:34:18 +0530

    sample commit

--- VDApp/src/com/sample/vdapp/MainActivity.java
+++ VDApp/src/com/sample/vdapp/MainActivity.java
@@ -7,9 +7,13 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	Button button;
+public class MainActivity extends Activity implements OnClickListener{
+	private Button button;
+	private EditText edt_uname,edt_password;
+	private boolean validateFlag;
 	 //Test comment
 	 //Test comment1
 	@Override
@@ -18,10 +22,11 @@ public class MainActivity extends Activity {
 		// Get the layout from video_main.xml
 		setContentView(R.layout.activity_main);
  
-		// Locate the button in activity_main.xml
-		button = (Button) findViewById(R.id.MyButton);
+		init();
+		
+		
  
-		// Capture button clicks
+		/*// Capture button clicks
 		button.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
  
@@ -30,13 +35,64 @@ public class MainActivity extends Activity {
 						VideoViewActivity.class);
 				startActivity(myIntent);
 			}
-		});
+		});*/
+	}
+	private void init() {
+		try {
+			edt_uname = (EditText) findViewById(R.id.edt_uname);
+			edt_password = (EditText) findViewById(R.id.edt_password);
+			
+			button = (Button) findViewById(R.id.MyButton);
+			button.setOnClickListener(this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.MyButton:
+			
+			String uname = edt_uname.getText().toString();
+			String password = edt_password.getText().toString();
+			
+			if (validateCredential(uname, password)) {
+				// Start NewActivity.class
+				Intent myIntent = new Intent(MainActivity.this,
+						VideoViewActivity.class);
+				startActivity(myIntent);
+			} else {
+				Toast.makeText(MainActivity.this, "Enter Valid Credentials", Toast.LENGTH_SHORT).show();
+			}			
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	private boolean validateCredential(String uname, String password) {
+		try {
+			if(uname.length() > 0 && password.length() > 0)
+			{
+				validateFlag = true;
+			}else{
+				validateFlag = false;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return validateFlag;
+		
 	}
 
 }
