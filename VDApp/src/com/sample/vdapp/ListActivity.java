d40aaf6d624b6ce2146c7a63e6c543ccaa899255
nitish.patel <nitish.patel@RIL15069CKB152.rolta.com>
2016-10-24 16:37:25 +0530

    commit

--- VDApp/src/com/sample/vdapp/ListActivity.java
+++ VDApp/src/com/sample/vdapp/ListActivity.java
@@ -40,7 +40,7 @@ public class ListActivity extends Activity{
 		return true;
 	}
 	
-	@Override
+	/*@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		// TODO Auto-generated method stub
 		switch (item.getItemId()) {
@@ -61,6 +61,31 @@ public class ListActivity extends Activity{
 			return true;
 		default:
 			return super.onMenuItemSelected(featureId, item);
+		}
+		
+	}*/
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.action_user_profile:
+			Intent i = new Intent(ListActivity.this,UserProfileActivity.class);
+			startActivity(i);
+			return true;
+		case R.id.action_video_list:
+			Intent intent = new Intent(ListActivity.this,ListActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.action_change_password:
+			Intent chnageIntent = new Intent(ListActivity.this,ChangePasswordMenuActivity.class);
+			startActivity(chnageIntent);
+			return true;
+		case R.id.action_logout:	
+			Toast.makeText(ListActivity.this, "logout", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 		
 	}
